9dd6ab610a11b9ae7c79cb36ab71cdb6e4430b7b
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-11-11 04:39:56 -0800

    trying again with a fresh commit

---
+++ discuss/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Discuss::Application.config.secret_key_base = '7854c27a8b127c310757e98d15316373fc0c54a425ecd91f317dad3e60541f4c5c9d6847bc5e6f0797c7e34da4f8a89fc4dbd42f306128bd945504e966bb707a'
